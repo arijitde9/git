d65a8df03398b698809b4ecd370746fc8ca37abc
arijitde9 <arijitde9@gmail.com>
2015-09-11 12:37:55 +0600

    EMS New

---
+++ EMS/src/com/test/cam/InitCam.java
@@ -0,0 +1,14 @@
+package com.test.cam;
+
+import javax.servlet.http.HttpServlet;
+
+public class InitCam extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5877637136265940671L;
+
+	
+	
+}
